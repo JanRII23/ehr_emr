983ef4ef806217be2f4eb8dacb58a3c05f388086
AtakaII <jrejasii@gmail.com>
2022-08-08 19:22:53 -0500

    starting off the ehr angular app with a task management system

---
+++ angularEHR_main/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TaskManagerComponent } from './task-manager/task-manager.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    TaskManagerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
