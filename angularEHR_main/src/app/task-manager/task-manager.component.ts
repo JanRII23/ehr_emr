23c6ab3e4733a0057c8aebdbebc7cc5c4df38764
AtakaII <jrejasii@gmail.com>
2022-08-09 16:40:13 -0500

    almost done with my basic angular to do ui

--- angularEHR_main/src/app/task-manager/task-manager.component.ts
+++ angularEHR_main/src/app/task-manager/task-manager.component.ts
@@ -1,5 +1,7 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ITask } from '../model/task';
 
 @Component({
   selector: 'app-task-manager',
@@ -9,15 +11,65 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class TaskManagerComponent implements OnInit {
 
   taskForm !: FormGroup;
-  tasks : any [] = [];
-  inprogress: any [] = [];
-  done: any[] = [];
+  tasks : ITask [] = [];
+  inprogress: ITask [] = [];
+  done: ITask[] = [];
+  updateIndex!:any;
+  isEditEnabled : boolean = false;
   constructor(private fb : FormBuilder) { }
 
   ngOnInit(): void {
     this.taskForm = this.fb.group({
       item : ['', Validators.required]
     })
+  }
+
+  addTask(){
+    this.tasks.push({
+      description: this.taskForm.value.item,
+      done: false
+    })
+    this.taskForm.reset();
+  }
+
+  onEdit(item:ITask, i : number){
+    this.taskForm.controls['item'].setValue(item.description);
+    this.updateIndex = i;
+    this.isEditEnabled = true;
+  }
+
+  updateTask(){
+
+    this.tasks[this.updateIndex].description = this.taskForm.value.item;
+    this.tasks[this.updateIndex].done = false;
+    this.taskForm.reset();
+    this.updateIndex = undefined;
+    this.isEditEnabled = false;
+  }
+
+  deleteTask(i: number){
+    this.tasks.splice(i, 1)
+  }
+
+  deleteInProgressTask(i: number){
+    this.inprogress.splice(i, 1)
+  }
+
+  deleteCompleteTask(i: number){
+    this.done.splice(i, 1)
+  }
+
+  drop(event: CdkDragDrop<ITask[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 
 }
