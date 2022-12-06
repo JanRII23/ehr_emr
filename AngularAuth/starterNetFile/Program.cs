91a1b254446eaaf2e703d308bd356018c4135437
AtakaII <jrejasii@gmail.com>
2022-12-05 18:02:07 -0600

    setup the .net file

---
+++ AngularAuth/starterNetFile/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// export DOTNET_ROOT=$HOME/dotnet  --> to enable dotnet
+
+// export PATH=$PATH:$HOME/dotnet   --> to enable dotnet
+
+Console.WriteLine("Hello, World!");
