bf9daefa8926a330cf12b4d89cd50ba0790474ac
AtakaII <jrejasii@gmail.com>
2022-12-14 18:24:04 -0600

    finished UI for sign up and login

--- angularEHR_main/src/app/components/signup/signup.component.ts
+++ angularEHR_main/src/app/components/signup/signup.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  type: string = "password"; //this is actually a built in feature of showing dots when setting as a password
+  isText: boolean = false;
+  eyeIcon: string = "visibility_off";
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  hideShowPass(){ //question marks are if logics and ! is opposite and this is refering to the specific instance
+      this.isText = !this.isText;
+      this.isText ? this.eyeIcon = "visibility" : this.eyeIcon = "visibility_off";
+      this.isText ? this.type = "text" : this.type = "password";
+  }
 }
+
+
