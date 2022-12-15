f0a6aa5dbbc63cfb70115fd18b29480860fa6a1e
AtakaII <jrejasii@gmail.com>
2022-12-15 16:11:09 -0600

    completed signup/login form validation

--- angularEHR_main/src/app/components/login/login.component.ts
+++ angularEHR_main/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import ValidateForm from 'src/app/helpers/validateForms';
+
 
 @Component({
   selector: 'app-login',
@@ -10,9 +13,24 @@ export class LoginComponent implements OnInit {
   type: string = "password"; //this is actually a built in feature of showing dots when setting as a password
   isText: boolean = false;
   eyeIcon: string = "visibility_off";
-  constructor() { }
+  loginForm! : FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password:['', Validators.required]
+    })
+  }
+
+  onSubmit(){
+
+    if (this.loginForm.valid){
+
+    }else{
+      ValidateForm.validateAllFormFields(this.loginForm);
+    }
+
   }
 
   hideShowPass(){ //question marks are if logics and ! is opposite and this is refering to the specific instance
--- angularEHR_main/src/app/components/signup/signup.component.ts
+++ angularEHR_main/src/app/components/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import ValidateForm from 'src/app/helpers/validateForms';
 
 @Component({
   selector: 'app-signup',
@@ -10,10 +12,31 @@ export class SignupComponent implements OnInit {
   type: string = "password"; //this is actually a built in feature of showing dots when setting as a password
   isText: boolean = false;
   eyeIcon: string = "visibility_off";
-  constructor() { }
+  signupForm! : FormGroup
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.signupForm = this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
   }
+
+
+  onSignup(){
+
+    if (this.signupForm.valid){
+
+    }else{
+      ValidateForm.validateAllFormFields(this.signupForm);
+    }
+
+  }
+
+
 
   hideShowPass(){ //question marks are if logics and ! is opposite and this is refering to the specific instance
       this.isText = !this.isText;
